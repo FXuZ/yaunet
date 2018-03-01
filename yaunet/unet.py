63661f6e7a7cdd451c6ec08a89f3925f68375b8a
Zhen Xu <xuzhen165@gmail.com>
2018-02-28 20:46:13 -0500

    Init commit

---
+++ yaunet/unet.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+from tensorflow import nn
+
+class Unet:
+    def __init__(self, n_channels=4, n_depth=4, n_class=2):
+        pass
+
+    def predict(self, image):
+        pass
+
+    def score(self, image, mask):
+        pass
+
+    def _build_net(self, n_channels=4, d_depth=4, n_class=2):
+        pass
