319a2472d00d1e81ecea59409aa4c71b01ff5bc9
Zhen Xu <xuzhen165@gmail.com>
2018-04-08 17:26:30 -0400

    Fixed the mask threshold bug
    now when resizing mask, it will be thresholded at the mean grayscale

--- yaunet/unet.py
+++ yaunet/unet.py
@@ -4,14 +4,13 @@ import os
 import csv
 import tensorflow as tf
 import numpy as np
-import pylab
+
 from skimage import io
 from skimage.transform import resize
 
 IMG_SIZE = (256, 256)
 IMG_KEY = 'image'
 MSK_KEY = 'mask'
-RESIZE_THRESH = 0.5
 
 def build_net(depth=5):
     inputs = {}
@@ -33,9 +32,9 @@ def build_net(depth=5):
         with tf.variable_scope('down{}'.format(i+1))as scope:
             layer0 = tf.layers.max_pooling2d(layers[-1][-1], pool_size=2, padding='same', strides=2)
             layer1 = tf.layers.conv2d(layer0, filters=n_channel, kernel_size=3,
-                                    strides=(1, 1), padding='same', activation=tf.nn.relu)
+                                      strides=(1, 1), padding='same', activation=tf.nn.relu)
             layer2 = tf.layers.conv2d(layer1, filters=n_channel, kernel_size=3,
-                                    strides=(1, 1), padding='same', activation=tf.nn.relu)
+                                      strides=(1, 1), padding='same', activation=tf.nn.relu)
         layers.append([layer0, layer1, layer2])
 
     # Up-convolve
@@ -47,15 +46,15 @@ def build_net(depth=5):
                                                 kernel_size=2, strides=(2, 2))
             up_layer1 = tf.concat([layers[depth - i - 2][-1], up_layer0], axis=3)
             up_layer2 = tf.layers.conv2d(up_layer1, filters=n_channel,
-                                        kernel_size=3, strides=(1, 1), padding='same')
+                                         kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu)
             up_layer3 = tf.layers.conv2d(up_layer2, filters=n_channel,
-                                        kernel_size=3, strides=(1, 1), padding='same')
+                                         kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu)
         layers.append([up_layer1, up_layer2, up_layer3])
 
     with tf.variable_scope('output') as scope:
         logits = tf.layers.conv2d(layers[-1][-1], filters=2, kernel_size=1, strides=(1, 1), padding='same')
         output = tf.nn.softmax(logits, axis=3)
-    truth = tf.placeholder(dtype=tf.int32, shape=(None, 256, 256), name='label')
+    truth = tf.placeholder(dtype=tf.int64, shape=(None, 256, 256), name='label')
     loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(truth, 2),
                                                           logits=output))
     inputs['feature'] = img
@@ -117,25 +116,32 @@ def import_dataset(data_dict):
     feature = np.stack([img['image'] for img in img_data])
     label = np.stack([img['mask'] for img in img_data])
     while True:
-        for i in range(feature.shape[0]):
-            yield feature[i: i+1], label[i: i+1]
+        for i in range(feature.shape[0] // 10):
+            yield feature[i: i+10], label[i: i+10]
     # return tf.data.Dataset.from_tensor_slices((feature, label))
+
+def get_all_data(data_dict):
+    key, img_data = zip(*data_dict.items())
+    feature = np.stack([img['image'] for img in img_data])
+    label = np.stack([img['mask'] for img in img_data])
+    return feature, label
 
 def resize_image(data_dict, size=IMG_SIZE):
     for key, img_data in data_dict.items():
         img_data[IMG_KEY] = resize(img_data[IMG_KEY], size).reshape(IMG_SIZE + (1,))
-        img_data[MSK_KEY] = (resize(img_data[MSK_KEY], size) > RESIZE_THRESH).astype(np.int32)
+        mask_resize = resize(img_data[MSK_KEY], size)
+        img_data[MSK_KEY] = (mask_resize > mask_resize.mean()).astype(np.int32)
     return data_dict
 
 def main():
     data = get_image_data('../../small', '../../stage1_train_labels.csv')
     print('=== data loaded')
     train_iter = import_dataset(resize_image(data))# .prefetch(10).batch(10)
-    # train_iter = train_set.make_one_shot_iterator()
+    all_feature, all_label = get_all_data(data)
+    test_feature, test_label = get_all_data(resize_image(get_image_data('../../small_test/', '../../stage1_train_labels.csv')))
     inputs, outputs, layers = build_net()
-    optim = tf.train.AdamOptimizer(learning_rate=0.0004)
+    optim = tf.train.GradientDescentOptimizer(learning_rate=0.01)
     train_op = optim.minimize(outputs['loss'])
-    print(tf.local_variables())
     print('=== start training')
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -153,16 +159,15 @@ def main():
                 if not (i % 10):
                     print("=== step {}".format(i))
                     print("loss: {}".format(sess.run(outputs['loss'], feed_dict={
-                        inputs['feature']: feature,
-                        inputs['label']: label
+                        inputs['feature']: all_feature,
+                        inputs['label']: all_label
                     })))
                     print("accuracy: {}".format(sess.run(outputs['accuracy'], feed_dict={
-                        inputs['feature']: feature,
-                        inputs['label']: label
+                        inputs['feature']: test_feature,
+                        inputs['label']: test_label
                     })))
             except tf.errors.OutOfRangeError:
                 break
 
 if __name__ == '__main__':
-    # data = get_image_data('../../training', '../../stage1_train_labels.csv')
     main()
